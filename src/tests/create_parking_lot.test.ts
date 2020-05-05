af4e0f350aeb9cd2a8563e0900d23dfc3633ff71
xameeramir <zameeramir@gmail.com>
2020-05-06 02:45:45 +0530

    more tests

--- src/tests/create_parking_lot.test.ts
+++ src/tests/create_parking_lot.test.ts
@@ -1,15 +1,21 @@
 import { create_parking_lot } from "../parkingLotFunctions";
 
-describe('create_parking_lot tests', () => {
+describe('create_parking_lot function tests', () => {
     it('should create 6 parking slots', async () => {
         let expectedOutput = 'Created parking lot with 6 slots';
         let actualOutput = await create_parking_lot(6);
         expect(actualOutput).toEqual(expectedOutput);
     });
 
-    it('should throw error when a valid number for slots is not passed', async () => {
+    it('should throw error when text is passed for slots number instead of a number', async () => {
         let expectedOutput = 'Error! Invalid parameter!';
         let actualOutput = await create_parking_lot('abc');
+        expect(actualOutput).toEqual(expectedOutput);
+    });
+
+    it('should throw error when null is passed for slots number instead of a number', async () => {
+        let expectedOutput = 'Error! Invalid parameter!';
+        let actualOutput = await create_parking_lot(null);
         expect(actualOutput).toEqual(expectedOutput);
     });
 });

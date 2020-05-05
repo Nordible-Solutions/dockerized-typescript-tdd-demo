7e00e35d70c017002c89f832895e3080c62fcce9
xameeramir <zameeramir@gmail.com>
2020-05-06 02:48:24 +0530

    descriptions as per tests

--- src/tests/park.test.ts
+++ src/tests/park.test.ts
@@ -1,38 +1,33 @@
 import { park, create_parking_lot } from "../parkingLotFunctions";
 
-// beforeEach(async () => {
-//     await create_parking_lot(6);
-// });
-
 describe('park function tests', () => {
 
-    it('should throw error when text is passed for slots number instead of a number', async () => {
+    it('should test uninitiated parking lot condition', async () => {
         let expectedOutput = 'Error, please initate parking lot';
         let actualOutput = await park('abc');
         expect(actualOutput).toEqual(expectedOutput);
     });
 
-    it('should allocate one slot to the car', async () => {
+    it('should allocate one slot to the vehicle', async () => {
         let expectedOutput = 'Allocated slot number: 1';
         await create_parking_lot(2);
         let actualOutput = await park('KA-01-HH-1234');
         expect(actualOutput).toEqual(expectedOutput);
     });
 
-    it('should throw error when text is passed for slots number instead of a number', async () => {
+    it('should allocate second slot to the vehicle', async () => {
         let expectedOutput = 'Allocated slot number: 2';
         let actualOutput = await park('abc');
         expect(actualOutput).toEqual(expectedOutput);
     });
-
     
-    it('should throw error when text is passed for slots number instead of a number', async () => {
+    it('should test threshold of the parking lot', async () => {
         let expectedOutput = 'No space for more vehicles';
         let actualOutput = await park(123);
         expect(actualOutput).toEqual(expectedOutput);
     });
 
-    it('should throw error when null is passed for vehicle registration number', async () => {
+    it('should test invalid parameters to the parking function', async () => {
         let expectedOutput = 'Error, vehicle registration number is needed';
         let actualOutput = await park(null);
         expect(actualOutput).toEqual(expectedOutput);

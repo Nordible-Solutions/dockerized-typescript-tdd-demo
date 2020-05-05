9d30196b29bd8e89d320b0fc7146d955b5ea72cf
xameeramir <zameeramir@gmail.com>
2020-05-06 02:13:28 +0530

    parking feature++

--- src/parkingLotFunctions.ts
+++ src/parkingLotFunctions.ts
@@ -1,37 +1,59 @@
-let parkingSlots = [];
+let vehiclesToPark = [];
+let availableParkingSlots = [];
 let lotSize = 0;
 
 /**
  * create_parking_lot command
  * @param howManyLots no. of slots in the parking
  */
-let create_parking_lot = async (howManyLots) => {
-    try {
-        lotSize = parseInt(howManyLots);
-    } catch (e) {
+export const create_parking_lot = async (howManyLots) => {
+    lotSize = parseInt(howManyLots);
+    if (isNaN(lotSize)) {
         return "Error! Invalid parameter!";
     }
 
     for (let i = 1; i <= lotSize; i++) {
-        parkingSlots.push(i);
+        availableParkingSlots.push(i);
     }
     return `Created parking lot with ${lotSize} slots`;
 }
 
+let park = async (vehicleNumber) => {
+    if (lotSize === vehiclesToPark.length) {
+        return `No space for more vehicles`;
+    } else if (lotSize === 0) {
+        return `Error, please initate parking lot`;
+    } else {
+        let parkingSlot = availableParkingSlots[0];
+        vehiclesToPark.push({
+            'slot': parkingSlot,
+            'vehicleNumber': vehicleNumber
+        });
+        availableParkingSlots.shift(); //available parking slots are reduced
+        return `Allocated slot number: ${parkingSlot}`
+    }
+}
+
 export const commandParser = async (input) => {
-    input = input.split(" ");
+    try {
 
-    switch (input[0]) {
-        case ('create_parking_lot'):
+        input = input.toLowerCase().split(" ");
+        let result;
 
-            try {
-                const result = await create_parking_lot(input[1]);
+        switch (input[0]) {
+            case ('create_parking_lot'):
+
+                result = await create_parking_lot(input[1]);
                 console.log(result);
-            } catch (e) {
-                console.log(`Error ${e}`);
-            }
-            break;
-        default:
-            console.log('Error in command input!');
+                break;
+            case ('park'):
+                result = await park(input[1].trim());
+                console.log(result);
+                break;
+            default:
+                console.log('Error in command input!');
+        }
+    } catch (e) {
+        console.log(`Error ${e}`);
     }
 }

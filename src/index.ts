edea1c0f8c499ed304ece77b7efbab067deacdb3
xameeramir <zameeramir@gmail.com>
2020-05-06 01:46:39 +0530

    parking commands

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ import { commandParser } from "./parkingLotFunctions";
 
 commandParser('create_parking_lot 6');
 commandParser('park KA-01-HH-1234');
+commandParser('park KA-01-HH-9999');
+commandParser('park KA-01-BB-0001');
+commandParser('park KA-01-HH-7777');
+commandParser('park KA-01-HH-2701');
+commandParser('park KA-01-HH-3141');
